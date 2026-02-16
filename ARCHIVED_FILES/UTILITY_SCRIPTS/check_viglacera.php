1886bd8db23bc5f67ed5f4ad3918c527063b17cb
thodh623 <dhtho1713@gmail.com>
2026-02-16 23:05:30 +0700

    Initial commit - DATN

--- ARCHIVED_FILES/UTILITY_SCRIPTS/check_viglacera.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-
-$db = new PDO('sqlite:database/database.sqlite');
-
-// Check Viglacera brand
-$result = $db->query("SELECT * FROM brands WHERE name = 'Viglacera'")->fetch(PDO::FETCH_ASSOC);
-echo "Brand Viglacera: " . json_encode($result, JSON_UNESCAPED_UNICODE) . "\n";
-?>
